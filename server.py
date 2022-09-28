6c699051134d0840e53b2f0bef721ed72d1f9841
ThianReinoso <sebasreinoso@gmail.com>
2022-09-28 12:48:45 -0500

    Prueba con SVR

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 #POSTMAN PARA PRUEBAS
 @app.route('/predict', methods=['GET'])
 def predict():
-    X_test = np.array([15,7.00,9,2.00,0,1,1,1,1,1,1,1,1,3,1,2,1,1,1,1,1,0,1,1,1,2])
+    X_test = np.array([15,8.00,10,2.00,8,2,9,3,6,5,3,5,7,6,5,8,4,5,8,4,5,8,3,5,5,5])
     prediction = model.predict(X_test.reshape(1,-1))
 
     return jsonify({'prediccion' : list(prediction)})
